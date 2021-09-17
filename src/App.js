3ab19a5fd3166f36d3761d32006d566fbc06e1ef
HAMMAD <hammadrafi14@gmail.com>
2021-09-18 01:00:45 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import Body from './Components/Body/Body';
 import Header from './Components/Header/Header';
 
 
+
 function App() {
-  const username= "HAMMAD"
+  const username = "HAMMAD"
   return (
     <>
-    <div style={{
-      backgroundColor:'lightblue',
-      display:'flex',
-      justifyContent:'center',
-      alignItems:'center',
-      width:'100vw',
-      height:'100vh'
+      <div style={{
+
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100vw',
+        height: '100vh'
       }}>
 
-      <Header username={username} />
-      <Body/>
-    
-    </div>
+        <Header username={username} />
+
+        <Body
+          text={`I was born in the year 18-- to a large fortune, endowed besides with excellent parts, inclined by nature to industry, fond of the respect of the wise and good among my fellowmen, and thus, as might have been supposed, with every guarantee of an honourable and distinguished future. And indeed the worst of my faults was a certain impatient gaiety of disposition, such as has made the happiness of many, but such as I found it.`}
+          size={200}
+        
+        />
+
+      </div>
     </>
   );
 }
--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,30 +1,44 @@
-import React from 'react'
+import react from 'react'
+import React, {useState} from 'react'
 
-function Body() {
+function Body(props) {
+    const {text,size} = props
+    let textmin = text.slice(0,size)
+    const[isbody, setState] = react.useState(false)
+    const textsrc = isbody ? text:textmin
+    const btntext = isbody? 'Readless':'ReadMore...'
+    
+    function TogglebtnState(){
+        setState(!isbody)
+    }
+
     return (
 
         <div style={{
-            backgroundColor: 'white',
+            backgroundColor: 'lightgray',
             height: 363,
             width: '30%',
             padding: '5%',
             textAlign: 'center',
             borderTopRightRadius: 10,
-            borderBottomRightRadius: 10
+            borderBottomRightRadius: 10,
+            
 
         }}>
 
-
             <h1 style={{ color: 'black', textAlign: 'center' }}>Bio</h1>
 
-            <p style={{ marginTop: '80', fontFamily: 'sans-serif', fontSize: 20, lineHeight: 1.5 }}>Dolore quis Lorem veniam enim. Commodo enim consectetur adipisicing ea excepteur voluptate pariatur nostrud. Culpa pariatur voluptate labore veniam quis deserunt ipsum. Nulla ea ut sunt dolore minim magna nostrud esse non qui ut eu. Duis consequat non eiusmod labore magna ex elit. Et est deserunt aliquip veniam velit incididunt labore officia.</p>
-        
+                <p style={{ marginTop: '80', fontFamily: 'sans-serif', fontSize: 20, lineHeight: 1.5 }}>{textsrc}
 
-            <div style={{display:'flex',justifyContent:   'space-between'}}>
+                <button onClick={TogglebtnState} style={{color:'blue',borderStyle:'none',backgroundColor:'lightgray'}}>{btntext}</button>
+            
+                </p>
+                {/* <div style={{display:'flex',justifyContent:   'space-between'}}>
 
-                <h3 style={{textDecorationLine:'underline'}}>View Skills</h3>
-                <h3>Joined: 15-08-2021</h3>
-            </div>
+                    <h3 style={{textDecorationLine:'underline'}}>View Skills</h3>
+                    <h3>Joined: 15-08-2021</h3>
+                </div> */}
+            
 
         </div>
 
